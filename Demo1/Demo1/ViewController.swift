d9ef0faf80dad6f4012dda1d2469d3282346375a
liuming <1969489778@qq.com>
2018-07-11 16:55:27 +0800

    镜像图片demo

---
+++ Demo1/Demo1/ViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ViewController.swift
+//  Demo1
+//
+//  Created by liuming on 2018/7/11.
+//  Copyright © 2018年 yoyo. All rights reserved.
+//
+
+import UIKit
+import GLKit
+import OpenGLES.ES2
+class ViewController: UIViewController {
+    
+    private var context:EAGLContext = EAGLContext.init(api:.openGLES2)!;
+    private var effect:GLKBaseEffect = GLKBaseEffect.init();
+    var count:GLuint = 0;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        setupConfig();
+        loadTexture();
+        initVertexCoordinates();
+    }
+    func setupConfig(){
+        
+        let glView:GLKView = self.view as! GLKView;
+        glView.context = context;
+        //设置渲染颜色格式
+        glView.drawableColorFormat = .RGBA8888;
+        //设置渲染深度
+        glView.drawableDepthFormat = .format24;
+        EAGLContext.setCurrent(context);
+        
+    }
+    func initVertexCoordinates()  {
+        
+//        [-0.5 0]   [0,0.5]
+        //前三位顶点坐标  后两位纹理坐标
+        let vertext:[GLfloat] = [
+            //左半部分
+            0, 1.0, 0.0,    0.5, 0.0, //右下
+            0, -1.0, 0.0,    0.5, 1.0, //右上
+            -1.0, -1.0, 0.0,    0, 1.0, //左上
+            
+            0.0, 1.0, 0.0,    0.5, 0.0, //右下
+            -1.0, -1.0, 0.0,   0, 1.0, //左上
+            -1.0, 1.0, 0.0,   0.0, 0.0, //左下
+            
+            //右半部分
+            1.0, 1.0, 0.0,     0.0, 0.0, //右下
+            1.0, -1.0, 0.0,    0, 1.0, //右上
+            0, -1.0, 0.0,     0.5, 1.0, //左上
+
+            1.0, 1.0, 0.0,    0.0, 0.0, //右下
+            0.0, -1.0, 0.0,   0.5, 1.0, //左上
+            0.0, 1.0, 0.0,   0.5, 0.0, //左下
+        ];
+        count = GLuint(vertext.count);
+        var buffer:GLuint = 0;
+        glGenBuffers(1, &buffer);
+        glBindBuffer(GLenum(GL_ARRAY_BUFFER), buffer);
+        //提交顶点数据
+        glBufferData(GLenum(GL_ARRAY_BUFFER), GLsizeiptr(MemoryLayout<GLfloat>.size * vertext.count) , vertext, GLenum(GL_STATIC_DRAW));
+        
+        //顶点
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.position.rawValue));
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.position.rawValue),
+                              3,
+                              GLenum(GL_FLOAT),
+                              GLboolean(GL_FALSE),
+                              GLsizei(MemoryLayout<GLfloat>.size * 5), nil);
+        
+        let offset :GLsizeiptr = MemoryLayout<GLfloat>.size * 3;
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.texCoord0.rawValue));
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.texCoord0.rawValue),
+                              2,
+                              GLenum(GL_FLOAT),
+                              GLboolean(GL_FALSE),
+                              GLsizei(MemoryLayout<GLfloat>.size * 5),
+                              UnsafeRawPointer.init(bitPattern: offset));
+        
+        
+    }
+    
+    func loadTexture() {
+        
+        let filePath :String = Bundle.main.path(forResource: "test", ofType: "jpg")!;
+        
+        do {
+            //[GLKTextureLoaderOriginBottomLeft:false]
+            let textureInfo:GLKTextureInfo = try GLKTextureLoader.texture(withContentsOfFile: filePath, options:nil);
+            effect.texture2d0.enabled = GLboolean(GL_TRUE);
+            effect.texture2d0.name = textureInfo.name;
+            
+        } catch let error {
+            
+            print("error is \(error.localizedDescription)");
+        }
+        
+    }
+    override     func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ViewController:GLKViewDelegate {
+    func glkView(_ view: GLKView, drawIn rect: CGRect) {
+        glClearColor(0.1, 0.5, 0.0, 1);
+        glClear(GLbitfield(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+        
+        effect.prepareToDraw();
+        glDrawArrays(GLenum(GL_TRIANGLES), 0, 12);
+        
+    }
+    
+    
+}
+
